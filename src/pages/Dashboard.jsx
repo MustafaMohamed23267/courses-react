1e46d91c54ab741612f7ab329cfcd004ad7c5f52
Mustafa Mohamed <120753752+MustafaMohamed23267@users.noreply.github.com>
2026-01-02 20:26:59 +0200

    update dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -331,7 +331,7 @@ const COLORS = ['#4f46e5', '#06b6d4', '#f43f5e', '#f59e0b', '#065f42'];
                 </Link>
 
                 <Link to='/addcategory' className={`${buttonstyle} space-x-2`}> 
-                <img src={add2} className={"w-5 md:fill-white"} />
+                <img src={add2} className={"w-5 md:fill-white max-sm:w-3"} />
                 <span className=' max-sm:hidden'>Add Category</span>
                 </Link>
                 
