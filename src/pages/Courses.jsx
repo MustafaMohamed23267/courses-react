a0a814b0c726a635bf7e762fb4872bc4846a11e5
Mustafa Mohamed <120753752+MustafaMohamed23267@users.noreply.github.com>
2026-01-04 15:33:56 +0200

    update courses

--- src/pages/Courses.jsx
+++ src/pages/Courses.jsx
@@ -30,7 +30,7 @@ export default function Courses()
                         
                     })
                      const response  = await res.json();
-                    setData(response.courses || []);
+                    setData(response.allcourses || []);
                     setLoading(false);
                     }
                     else{
